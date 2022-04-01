80274bcd3e56168bdd558237322446e7c7e8114c
Thomas Tran Dinh <tdt7384@gmail.com>
2022-04-01 14:12:35 -0400

    fonction Add product

--- routes/homeController.js
+++ routes/homeController.js
@@ -12,16 +12,6 @@ exports.getIndex = (req, res) => {
         });
 };
 
-// exports.getIndex = (req, res) => {
-//     const products = [{
-//         code: "test code",
-//         description: "test description",
-//         price: "test price"
-//     }];
-
-//     res.render("index", { title: "Product Database System", products: products });
-// };
-
 
 exports.addProduct = (req, res) => {
     res.render("new", { title: "Add product", product: new Product() });
@@ -46,30 +36,4 @@ exports.saveProduct = (req, res) => {
 };
 
 
-exports.findOneProduct = (req, res) => {
-
-};
-
-
-// exports.saveProduct = (req, res) =>{
-//     const code = req.body.code;
-//     const description = req.body.description;
-//     const price = req.body.price;
-
-//     const newProduct = new Product({code : code, description : description, price : price}); 
-//     newProduct.save()
-//     .then(()=>{
-//         res.render("index")
-//     })
-//     .catch((error)=>{
-//         console.log(error)
-//     })
-// };
-
-// exports.getNew = (req, res)=>{
-//     res.render("new");
-// };
-
-// exports.getSearch = (req, res)=>{
-//     res.render("search");
-// }
+exports.findOneProduct = (req, res) => {};
--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("./homeController");
 
-// router.get("/", homeController.getIndex);
+
 router.get("/", homeController.getIndex); //Page d'accueil. Affiche toutes les données entrées. Revient sur cette page à chaque ajout ou modification de la base de données
 router.get("/product/new", homeController.addProduct); //affiche le formulaire
 router.post("/", homeController.saveProduct); //valide les champs entrés dans le formulaire, sauve les bases de données et retourne vers l'index
