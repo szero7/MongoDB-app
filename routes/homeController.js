46ca024645570d9797d915c7b88ae6ea74dce647
Thomas Tran Dinh <tdt7384@gmail.com>
2022-04-01 13:21:36 -0400

    ajout de données et retour vers index

--- routes/homeController.js
+++ routes/homeController.js
@@ -3,34 +3,53 @@
 const Product = require("../models/product");
 
 exports.getIndex = (req, res) => {
-    const products = [{
-        code: "test code",
-        description: "test description",
-        price: "test price"
-    }];
-
-    res.render("index", { title: "Product Database System", products: products });
-};
-
-exports.addProduct = (req, res) => {
-    res.render("new", { title: "Add product" });
-};
-
-exports.saveProduct = (req, res) => {
-
-    const newProduct = new Product({
-        code: req.body.code,
-        description: req.body.description,
-        price: req.body.price
-    });
-    newProduct.save()
+    Product.find({})
         .then(product => {
-            res.redirect("/"); // res.redirect(`/${products._id}`);
+            res.render("index", { title: "Product Database System", products: product });
         })
         .catch(error => {
             console.log(error);
         });
 };
+
+// exports.getIndex = (req, res) => {
+//     const products = [{
+//         code: "test code",
+//         description: "test description",
+//         price: "test price"
+//     }];
+
+//     res.render("index", { title: "Product Database System", products: products });
+// };
+
+
+exports.addProduct = (req, res) => {
+    res.render("new", { title: "Add product", product: new Product() });
+};
+
+
+exports.saveProduct = (req, res) => {
+
+    const product = new Product({
+        code: req.body.code,
+        description: req.body.description,
+        price: req.body.price
+    });
+    product.save()
+        .then(product => {
+            res.redirect("/"); // res.redirect(`/${products._id}`);
+        })
+        .catch(error => {
+            console.log(error);
+            res.render("new", { product: product });
+        });
+};
+
+
+exports.findOneProduct = (req, res) => {
+
+};
+
 
 // exports.saveProduct = (req, res) =>{
 //     const code = req.body.code;
--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("./homeController");
 
-router.get("/", homeController.getIndex);
+// router.get("/", homeController.getIndex);
+router.get("/", homeController.getIndex); //Page d'accueil. Affiche toutes les données entrées. Revient sur cette page à chaque ajout ou modification de la base de données
 router.get("/product/new", homeController.addProduct); //affiche le formulaire
-router.post("/product/new", homeController.saveProduct); //valide les champs entrés dans le formulaire, sauve les bases de données et affiche l'index
-// router.get("/index", homeController.getIndex);
-// router.get("/new", homeController.getNew);
-// router.get("/search", homeController.getSearch);
+router.post("/", homeController.saveProduct); //valide les champs entrés dans le formulaire, sauve les bases de données et retourne vers l'index
+
+router.get("/:id", homeController.findOneProduct);
+
 
 
 
